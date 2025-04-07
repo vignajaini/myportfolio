d7685cde9e29efce944250ff5096dd009604ac09
j.vigna <jainivigna4@gmail.com>
2025-04-07 17:04:22 +0530

    new page called skills is created

---
+++ @latest/src/Skills.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Skills.css";
+
+function Skills() {
+  return (
+    <div id="Skills" className="skills-section">
+    <h2>Skills will be listed here</h2>
+  </div>
+  );
+}
+
+export default Skills;
