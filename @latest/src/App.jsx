e215dbcede61ca8364946edaf0c2c380ce25c653
j.vigna <jainivigna4@gmail.com>
2025-04-07 17:05:15 +0530

    included skills page for navigation

--- @latest/src/App.jsx
+++ @latest/src/App.jsx
@@ -3,7 +3,9 @@ import Home from "./Home";
 import About from "./About";
 import Projects from "./Projects";
 import Contact from "./Contact";
+import Skills from "./Skills";
 import "./App.css";
+
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
           <ul>
             <li><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
+            <li><a href="#Skills">Skills</a></li>
             <li><a href="#projects">Projects</a></li>
             <li><a href="#contact">Contact</a></li>
           </ul>
@@ -22,6 +25,7 @@ function App() {
 
       <Home />
       <About />
+      <Skills />
       <Projects />
       <Contact />
 
